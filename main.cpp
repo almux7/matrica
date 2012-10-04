21cde087bde19902757d3659713c074235ba7a63
Alma Galminiene <almux7@gmail.com>
2012-10-04 10:38:46 +0200

    Padariau teisingai

---
+++ main.cpp
@@ -0,0 +1,12 @@
+<SCRIPT type="text/javascript" language="JavaScript">
+var sk1 = 10;
+var sk2 = 20;
+var sum = sk1 + sk2;
+var skirt = sk1 - sk2;
+var sand = sk1 * sk2;
+var dal = sk1 / sk2;
+document.write(sk1+"+"+sk2+"="+sum+"<br>");
+document.write(sk1+"-"+sk2+"="+skirt+"<br>");
+document.write(sk1+"*"+sk2+"="+sand+"<br>");
+document.write(sk1+"/"+sk2+"="+dal+"<br>");
+</SCRIPT>
